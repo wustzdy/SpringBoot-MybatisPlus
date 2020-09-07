49671921e6640afd558a7a9b89129019369e50fb
zhudayang <zhudayang@sensetime.com>
2020-09-07 18:13:52 +0800

    add mysql

--- src/main/resources/iam-changelogs/V1.0.0/create_iam_zdy_user_table.sql
+++ src/main/resources/iam-changelogs/V1.0.0/create_iam_zdy_user_table.sql
@@ -3,7 +3,7 @@
 --changeset zhudayang:20200905-01
 CREATE TABLE `zdy_user` (
     `id` int (11) NOT NULL AUTO_INCREMENT,
-    `user_name1` varchar (60) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-    `pass_word1` varchar (60) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+    `user_name` varchar (60) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+    `pass_word` varchar (60) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE =InnoDB DEFAULT CHARSET =utf8mb4 COLLATE =utf8mb4_unicode_ci COMMENT '用户账户表';
