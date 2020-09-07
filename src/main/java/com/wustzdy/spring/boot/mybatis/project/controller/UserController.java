b15f6a550e292232cb7abc724d3d618240934df7
zhudayang <zhudayang@sensetime.com>
2020-09-07 18:26:03 +0800

    optimize

--- src/main/java/com/wustzdy/spring/boot/mybatis/project/controller/UserController.java
+++ src/main/java/com/wustzdy/spring/boot/mybatis/project/controller/UserController.java
@@ -2,6 +2,7 @@ package com.wustzdy.spring.boot.mybatis.project.controller;
 
 import com.wustzdy.spring.boot.mybatis.project.model.User;
 import com.wustzdy.spring.boot.mybatis.project.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,14 +13,13 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("zdy/user")
 public class UserController {
-    @Resource
-    private HelloService helloService;
-    @Resource
+
+    @Autowired
     private UserService userService;
 
     @RequestMapping(value = "/test", method = {RequestMethod.GET})
     public String getTest() {
-        return helloService.getTest();
+        return "helloService.getTest()";
     }
 
     @RequestMapping(value = "/createUser", method = RequestMethod.POST)
